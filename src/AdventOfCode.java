e5da26387de5b97e2c6b009f13cdda26511e38ba
Sarah Brinckmann <sarah.brinckmann@thoughtworks.com>
2022-11-30 22:37:03 +0100

    initial setup for day 1

--- src/AdventOfCode.java
+++ src/AdventOfCode.java
@@ -1,26 +1,40 @@
 package src;
 
+import src.utils.FileReader;
+
+import java.io.IOException;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class AdventOfCode {
 
-    static private final String FILE_PATH_FORMAT = "./data/advent/%s_input";
+    static private final String FILE_PATH_FORMAT = "./data/advent/%s_input.txt";
     private static final String[] TITLE = {"first", "second"};
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int dayOfAdvent = getDayOfAdvent();
 
-        if (dayOfAdvent > TITLE.length) {
-            System.out.println(String.format("Code for day %s not implemented yet. Try again later!", dayOfAdvent));
-            return;
-        }
-        
+        System.out.println(String.format("Day %s - %s", dayOfAdvent, TITLE[dayOfAdvent - 1]));
+
         /**
          * process here
          */
+        switch (dayOfAdvent) {
+            case 1:
+                System.out.println("Call result calculation here");
+                break;
+            default:
+                System.out.println(String.format("Code for day %s not implemented yet. Try again later!", dayOfAdvent));
+                return;
+        }
 
-        System.out.println(String.format("Day %s - %s", dayOfAdvent, TITLE[dayOfAdvent - 1]));
+        // test reading input file
+        List<String> lines = FileReader.readFileToLines(getInputFilename(dayOfAdvent));
+        for (String line : lines) {
+            System.out.println(line);
+        }
+
         var result = "";
         System.out.println("Result: " + result);
     }
@@ -44,7 +58,7 @@ public class AdventOfCode {
         return dayOfAdvent;
     }
 
-    private String getInputFilename(int dayOfAdvent) {
+    private static String getInputFilename(int dayOfAdvent) {
         String day = dayOfAdvent < 10 ? "0" + dayOfAdvent : String.valueOf(dayOfAdvent);
         return String.format(FILE_PATH_FORMAT, day);
     }
---
+++ src/dayofadvent/Day_1.java
@@ -0,0 +1,18 @@
+package src.dayofadvent;
+
+public class Day_1 {
+
+    public Day_1(String inputFile) {
+        readInput(inputFile);
+    }
+
+    public void calcResult() {
+        /**
+         * process here
+         */
+    }
+
+    private void readInput(String inputFile) {
+
+    }
+}
